841de19c3412effc476b32306028163362af93ec
Ажархан Олжабай <azharkhanolzhabai@MacBook-Air-Azarhan.local>
2025-05-13 23:26:16 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,219 @@
+document.querySelector('.burger').addEventListener("click", function(){
+    document.querySelector('.main').classList.toggle("active");
+});
+
+
+
+// const btn1 = document.querySelector('.bir');
+// const btn2 = document.querySelector('.eki');
+// const btn3 = document.querySelector('.ush');
+// const btn4 = document.querySelector('.tort');
+// const btn5 = document.querySelector('.bes');
+// const btn6 = document.querySelector('.alty');
+// const btn7 = document.querySelector('.zheti');
+// const btn8 = document.querySelector('.sekiz');
+// const btn9 = document.querySelector('.tokuz');
+// const btn10 = document.querySelector('.on');
+// const btn11 = document.querySelector('.onbir');
+// const btn12 = document.querySelector('.oneki');
+// const cart = document.querySelector('.cart');
+// btn1.addEventListener("click", function(){
+//     cart.innerHTML = `
+//         <p class="cart">Cart</p>
+//         <div style="background-color: rgb(232, 232, 232); color: black; position:absolute; top: 50px; left: 1200px; height: 100px; padding: 30px;">
+//             <h2>Your Rental Cart</h2>
+//             <ul style="margin-bottom: 10px;">
+//                 <li>1984 - 3 day(s) - $1.50</li>
+//             </ul>
+//         </div>
+//     `;
+// });
+// btn2.addEventListener("click", function(){
+//     cart.innerHTML = `
+//         <p class="cart">Cart</p>
+//         <div style="background-color: rgb(232, 232, 232); color: black; position:absolute; top: 55px; left: 1200px; height: 100px; padding: 30px;">
+//             <h2>Your Rental Cart</h2>
+//             <ul style="margin-bottom: 10px;">
+//                 <li>The Hobbit - 3 day(s) - $2.00</li>
+//             </ul>
+//         </div>
+//     `;
+// });
+// btn3.addEventListener("click", function(){
+//     cart.innerHTML = `
+//         <p class="cart">Cart</p>
+//         <div style="background-color: rgb(232, 232, 232); color: black; position:absolute; top: 50px; left: 1200px; height: 100px; padding: 30px;">
+//             <h2>Your Rental Cart</h2>
+//             <ul style="margin-bottom: 10px;">
+//                 <li>To Kill a Mockingbird - 3 day(s) - $2.50</li>
+//             </ul>
+//         </div>
+//     `;
+// });
+// btn4.addEventListener("click", function(){
+//     cart.innerHTML = `
+//         <p class="cart">Cart</p>
+//         <div style="background-color: rgb(232, 232, 232); color: black; position:absolute; top: 50px; left: 1200px; height: 100px; padding: 30px;">
+//             <h2>Your Rental Cart</h2>
+//             <ul style="margin-bottom: 10px;">
+//                 <li>Pride and Prejudicet - 3 day(s) - $3.00</li>
+//             </ul>
+//         </div>
+//     `;
+// });
+// btn5.addEventListener("click", function(){
+//     cart.innerHTML = `
+//         <p class="cart">Cart</p>
+//         <div style="background-color: rgb(232, 232, 232); color: black; position:absolute; top: 50px; left: 1200px; height: 100px; padding: 30px;">
+//             <h2>Your Rental Cart</h2>
+//             <ul style="margin-bottom: 10px;">
+//                 <li>The Moby-Dick - 3 day(s) - $1.00</li>
+//             </ul>
+//         </div>
+//     `;
+// });
+// btn6.addEventListener("click", function(){
+//     cart.innerHTML = `
+//         <p class="cart">Cart</p>
+//         <div style="background-color: rgb(232, 232, 232); color: black; position:absolute; top: 50px; left: 1200px; height: 100px; padding: 30px;">
+//             <h2>Your Rental Cart</h2>
+//             <ul style="margin-bottom: 10px;">
+//                 <li>War and Peace - 3 day(s) - $1.50</li>
+//             </ul>
+//         </div>
+//     `;
+// });
+// btn7.addEventListener("click", function(){
+//     cart.innerHTML = `
+//         <p class="cart">Cart</p>
+//         <div style="background-color: rgb(232, 232, 232); color: black; position:absolute; top: 50px; left: 1200px; height: 100px; padding: 30px;">
+//             <h2>Your Rental Cart</h2>
+//             <ul style="margin-bottom: 10px;">
+//                 <li>The Great Gatsby - 3 day(s) - $2.00</li>
+//             </ul>
+//         </div>
+//     `;
+// });
+// btn8.addEventListener("click", function(){
+//     cart.innerHTML = `
+//         <p class="cart">Cart</p>
+//         <div style="background-color: rgb(232, 232, 232); color: black; position:absolute; top: 50px; left: 1200px; height: 100px; padding: 30px;">
+//             <h2>Your Rental Cart</h2>
+//             <ul style="margin-bottom: 10px;">
+//                 <li>The Catcher in the Rye - 3 day(s) - $2.50</li>
+//             </ul>
+//         </div>
+//     `;
+// });
+// btn9.addEventListener("click", function(){
+//     cart.innerHTML = `
+//         <p class="cart">Cart</p>
+//         <div style="background-color: rgb(232, 232, 232); color: black; position:absolute; top: 50px; left: 1200px; height: 100px; padding: 30px;">
+//             <h2>Your Rental Cart</h2>
+//             <ul style="margin-bottom: 10px;">
+//                 <li>Hobbit - 3 day(s) - $2.10</li>
+//             </ul>
+//         </div>
+//     `;
+// });
+// btn10.addEventListener("click", function(){
+//     cart.innerHTML = `
+//         <p class="cart">Cart</p>
+//         <div style="background-color: rgb(232, 232, 232); color: black; position:absolute; top: 50px; left: 1200px; height: 100px; padding: 30px;">
+//             <h2>Your Rental Cart</h2>
+//             <ul style="margin-bottom: 10px;">
+//                 <li>The - 3 day(s) - $4.00</li>
+//             </ul>
+//         </div>
+//     `;
+// });
+// btn11.addEventListener("click", function(){
+//     cart.innerHTML = `
+//         <p class="cart">Cart</p>
+//         <div style="background-color: rgb(232, 232, 232); color: black; position:absolute; top: 50px; left: 1200px; height: 100px; padding: 30px;">
+//             <h2>Your Rental Cart</h2>
+//             <ul style="margin-bottom: 10px;">
+//                 <li>TWO - 3 day(s) - $1.00</li>
+//             </ul>
+//         </div>
+//     `;
+// });
+// btn12.addEventListener("click", function(){
+//     cart.innerHTML = `
+//         <p class="cart">Cart</p>
+//         <div style="background-color: rgb(232, 232, 232); color: black; position:absolute; top: 50px; left: 1200px; height: 100px; padding: 30px;">
+//             <h2>Your Rental Cart</h2>
+//             <ul style="margin-bottom: 10px;">
+//                 <li>The Hobbit - 3 day(s) - $2.20</li>
+//             </ul>
+//         </div>
+//     `;
+// });
+
+
+document.querySelector(".bir").addEventListener("click", function(){
+    addToCart("1984", 1.5);
+});
+document.querySelector(".eki").addEventListener("click", function(){
+    addToCart("The Hobbit", 1.5);
+});
+document.querySelector(".ush").addEventListener("click", function(){
+    addToCart("To Kill a Mockingbird", 1.5);
+});
+document.querySelector(".tort").addEventListener("click", function(){
+    addToCart("Pride and Prejudice", 1.5);
+});
+document.querySelector(".bes").addEventListener("click", function(){
+    addToCart("Moby-Dick", 1.5);
+});
+document.querySelector(".alty").addEventListener("click", function(){
+    addToCart("War and Peace", 1.5);
+});
+document.querySelector(".zheti").addEventListener("click", function(){
+    addToCart("The Great Gatsby", 1.5);
+});
+document.querySelector(".sekiz").addEventListener("click", function(){
+    addToCart("The Catcher in the Rye", 1.5);
+});
+document.querySelector(".tokuz").addEventListener("click", function(){
+    addToCart("lorem", 1.5);
+});
+document.querySelector(".on").addEventListener("click", function(){
+    addToCart("OOne", 1.5);
+});
+document.querySelector(".onbir").addEventListener("click", function(){
+    addToCart("TWO", 1.5);
+});
+document.querySelector(".oneki").addEventListener("click", function(){
+    addToCart("Sony", 1.5);
+});
+
+
+let cart = [];
+let total = 0;
+
+function addToCart(name, price) {
+    cart.push({ name: name, price: price });
+    showCart();
+};
+
+function showCart() {
+let box = document.querySelector(".cart-box");
+if (!box) {
+    box = document.createElement("div");
+    box.className = "cart-box";
+    document.body.appendChild(box);
+};
+
+box.innerHTML = "<h3>Your Rental Cart</h3>";
+total = 0;
+
+for (let i = 0; i < cart.length; i++) {
+    box.innerHTML += `
+        <p>${cart[i].name} - $${cart[i].price}</p>
+    `;
+    total += cart[i].price;
+};
+
+box.innerHTML += `<p><b>Total: $${total.toFixed(2)}</b></p>`;
+};
